70721c4bc2ad6142df2565c73860d1268a9a04fb
parveen-jangir <jangirparveen95@gmail.com>
2026-05-04 07:02:41 +0530

    settings pages now functional

---
+++ SquareLine_project_files/SquareLine_Project_events.py
@@ -0,0 +1,52 @@
+
+def settingPassReady(event_struct):
+    return
+
+
+def scr6Settings(event_struct):
+    return
+
+
+def updSpdDorTimer(event_struct):
+    return
+
+
+def UpdAutoReLockTimer(event_struct):
+    return
+
+
+def updSprayDuration(event_struct):
+    return
+
+
+def togleSpeedDoor(event_struct):
+    return
+
+
+def callModeA(event_struct):
+    return
+
+
+def updSettingsPin(event_struct):
+    return
+
+
+def updateDate(event_struct):
+    return
+
+
+def updateTime(event_struct):
+    return
+
+
+def resetSystem(event_struct):
+    return
+
+
+def callModeB(event_struct):
+    return
+
+
+def callModeC(event_struct):
+    return
+
